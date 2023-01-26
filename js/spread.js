16b44b9cbb3ab661beb54bc3afc6bacb49e57cd8
Anikkhan-web <anikahmed.ef@gmail.com>
2023-01-26 15:50:57 +0600

    first commit

---
+++ js/spread.js
@@ -0,0 +1,7 @@
+const max = Math.max(12, 23, 45, 65, 878)
+
+const numbers = [12, 23, 45, 65, 878]
+
+const largest = Math.max(...numbers)
+console.log(...numbers)
+console.log(largest);
---
+++ js/template.js
@@ -0,0 +1,12 @@
+const a = "ac"
+const b = "bc"
+const c = "cc"
+const newSummary = ` sum of ${a} and ${b} \n is: ${a + b}`
+// console.log(newSummary);
+
+const anik = i => {
+    return i + 20
+}
+
+const result = anik(20)
+console.log(result);
