799b74640d99c716d62c9f5f73e86a03f62ab41d
Anikkhan-web <anikahmed.ef@gmail.com>
2023-01-26 15:54:40 +0600

    u

--- js/spread.js
+++ js/spread.js
@@ -3,5 +3,12 @@ const max = Math.max(12, 23, 45, 65, 878)
 const numbers = [12, 23, 45, 65, 878]
 
 const largest = Math.max(...numbers)
-console.log(...numbers)
-console.log(largest);
+// console.log(...numbers)
+// console.log(largest);
+
+const fish = {
+    name: 'king hilsa',
+    color: 'silver'
+}
+const name = fish.name;
+console.log(name);
